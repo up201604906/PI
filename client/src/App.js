1fb806b59b2dffe682a236d5660ad9642e91e1fc
ntsayzacarias <ntsayyzacarias@gmail.com>
2024-03-14 14:37:58 +0000

    containers now communicating!

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   }
 
   callAPI() {
-    fetch("http://localhost:9000/testServer")
+    fetch("/api")
       .then(res => res.text())
       .then(res => this.setState({ apiResponse: res }))
       .catch(err => err);
