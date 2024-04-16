7a03adfaa8da5bdba8ae75f152db7bf85502f741
Rui Soares <rui.rprs3@gmail.com>
2024-04-16 11:22:41 +0100

    added frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import { AuthProvider } from './contexts/AuthContext'; 
+import {AuthProvider} from './contexts/AuthContext';
 import './styles/App.css'
 import ProtectedRoute from "./components/components/common/ProtectedRoute";
 import Home from './components/pages/Home';
@@ -9,6 +9,7 @@ import Resources from "./components/pages/Resources";
 import CreateResource from "./components/pages/CreateResource";
 import MyArticles from "./components/pages/articles/MyArticles";
 import CreateArticle from "./components/pages/articles/CreateArticle";
+import Login from "./components/components/auth/Login";
 
 class App extends Component {
     constructor(props) {
@@ -35,21 +36,16 @@ class App extends Component {
                         <Topnav/>
                         <div id={"body"}>
                             <Routes>
-                                <Route path="/" element={
-                                    <Home className={"h-100"}/>
-                                    // <p className="App-intro">Server response: {this.state.apiResponse}</p>
-                                }/>
+                                <Route path="/" element={<Home className={"h-100"}/>}/>
                                 <Route path="/inventory/resources" element={
                                     <ProtectedRoute>
-                                        <Resources />
+                                        <Resources/>
                                     </ProtectedRoute>
-                                    
-                                }/> {}
-                                <Route path="/inventory/createResource" element={
-                                    <CreateResource />
                                 }/>
-                                <Route path="/myArticles" element={<MyArticles />} />  
-                                <Route path="/createArticle" element={<CreateArticle />} /> 
+                                <Route path="/inventory/createResource" element={<CreateResource/>}/>
+                                <Route path="/myArticles" element={<MyArticles/>}/>
+                                <Route path="/createArticle" element={<CreateArticle/>}/>
+                                <Route path="/login" element={<Login/>}/>
                             </Routes>
                         </div>
                     </div>
