711de73f667c8f145d7539b60c3924cf0c596e91
iamdianamartins <93086977+iamdianamartins@users.noreply.github.com>
2024-05-08 02:18:41 +0100

    table font size decrease

--- backend/api/src/controllers/wishlist_controller.js
+++ backend/api/src/controllers/wishlist_controller.js
@@ -36,26 +36,6 @@ const updateResourceInWishlist = async (req, res) => {
     }
 }
 
-/*
-async function add_item_to_wishlist(user_name, resource_name, potential_resource_name, description, category, supplier, price, priority, quantity) {
-    try {
-        if (resource_name !== "null") {
-            const user_id = (await pool.query(`SELECT id FROM users WHERE name = $1`, [user_name])).rows[0].id;
-            const resource_id = (await pool.query(`SELECT id FROM resources WHERE name = $1`, [resource_name])).rows[0].id;
-            await pool.query(`UPDATE resources SET supplier = $1, price = $2, priority = $3 WHERE id = $4`, [supplier, price, priority, resource_id]);
-            await pool.query(`INSERT INTO wishlist (user_id, resource_id, quantity) VALUES ($1, $2, $3)`, [user_id, resource_id, quantity]);
-        } else {
-            const user_id = (await pool.query(`SELECT id FROM users WHERE name = $1`, [user_name])).rows[0].id;
-            const potential_resource_id = (await pool.query(`INSERT INTO potential_resources (name, description, category, supplier, price, priority) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`, [potential_resource_name, description, category, supplier, price, priority])).rows[0].id;
-            await pool.query(`INSERT INTO wishlist (user_id, potential_resource_id, quantity) VALUES ($1, $2, $3)`, [user_id, potential_resource_id, quantity]);
-        }
-    } catch (error) {
-        console.error("Error adding item to wishlist:", error);
-        throw error;
-    }
-}
-*/
-
 const addResourceToWishlist = async (req, res) => {
     try {
         const { user_name, resource_name, potential_resource_name, description, category, supplier, price, priority, quantity } = req.body;
