f3ef79c9126f805b981639b816be66abf8624da5
ntsayzacarias <ntsayyzacarias@gmail.com>
2024-05-16 23:04:32 +0100

    fixed my projects page

--- backend/api/src/routes/projects.js
+++ backend/api/src/routes/projects.js
@@ -4,10 +4,11 @@ const projectController = require('../controllers/project_controller');
 
 router.get('/assigned/:userId', projectController.getAssignedProjects);
 
-router.get('/', projectController.getAllAProjects);
-router.get('/:id', projectController.getProjectById);
 router.post('/create', projectController.createProject);
 router.get("/types", projectController.getProjectTypes);
 router.get("/statuses", projectController.getProjectStatuses);
+router.get('/', projectController.getAllAProjects);
+router.get('/:id', projectController.getProjectById);
+
 
 module.exports = router;
