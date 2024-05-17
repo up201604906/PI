186308e718fd93fe0d1d5a8f3728d46bc9035973
ntsayzacarias <ntsayyzacarias@gmail.com>
2024-05-17 03:43:50 +0100

    implemented import

--- client/src/components/pages/articles/BibTeXImportPage.js
+++ client/src/components/pages/articles/BibTeXImportPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import bibtexParse from 'bibtex-parse-js'; // Ensure you have this installed
+import bibtexParse from 'bibtex-parse-js';
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../../contexts/AuthContext';
 import "../../../styles/ImportPage.css";
 
 const BibTeXImportPage = () => {
@@ -9,7 +10,9 @@ const BibTeXImportPage = () => {
     const [parsedData, setParsedData] = useState(null);
     const [additionalData, setAdditionalData] = useState(null);
     const [isEditing, setIsEditing] = useState(false);
+    const [isCurrentUserOwner, setIsCurrentUserOwner] = useState(false); // State to track if current user is the author
     const navigate = useNavigate();
+    const { currentUser } = useAuth();
 
     const handleFileUpload = (event) => {
         const file = event.target.files[0];
@@ -27,21 +30,32 @@ const BibTeXImportPage = () => {
         try {
             const response = await fetch(`https://api.crossref.org/works/${doiInput}`);
             const data = await response.json();
-            const entryTags = {
-                title: data.message.title[0],
-                author: data.message.author.map(a => `${a.given} ${a.family}`).join(', '),
-                year: data.message['published-print'] ? data.message['published-print']['date-parts'][0][0] : data.message['published-online']['date-parts'][0][0],
-                type: data.message.type,
-                doi: doiInput,
-                publisher: data.message.publisher,
-                url: data.message.URL,
-            };
+            const entryTags = parseApiResponse(data.message);
             setParsedData(entryTags);
             setAdditionalData(data.message);
             setIsEditing(true);
         } catch (error) {
             console.error('Error fetching DOI data:', error);
         }
+    };
+
+    const parseApiResponse = (message) => {
+        const authors = message.author.map(a => `${a.given} ${a.family}`).join(', ');
+        const year = message['published-print'] ? message['published-print']['date-parts'][0][0] : message['published-online']['date-parts'][0][0];
+        return {
+            title: message.title[0],
+            author: authors,
+            year: year,
+            type: message.type,
+            doi: message.DOI,
+            publisher: message.publisher,
+            url: message.URL,
+            journal: message['container-title'] ? message['container-title'][0] : '',
+            volume: message.volume || '',
+            number: message.issue || '',
+            pages: message.page || '',
+            keywords: message.subject ? message.subject.join(', ') : '',
+        };
     };
 
     const handleInputChange = (event) => {
@@ -55,8 +69,13 @@ const BibTeXImportPage = () => {
     const handleSubmit = async () => {
         const articleData = {
             ...parsedData,
-            authors: parsedData.author.split(',').map(a => a.trim())
+            authors: parsedData.author ? parsedData.author.split(',').map(a => a.trim()) : []
         };
+
+        if (isCurrentUserOwner && currentUser) {
+            articleData.userId = currentUser;
+        }
+
         try {
             const response = await fetch('http://localhost:4000/articles/create', {
                 method: 'POST',
@@ -64,7 +83,7 @@ const BibTeXImportPage = () => {
                 body: JSON.stringify(articleData)
             });
             if (response.ok) {
-                navigate('/a');
+                navigate('/myArticles/' + currentUser);
             } else {
                 console.error('Error creating article');
             }
@@ -101,9 +120,13 @@ const BibTeXImportPage = () => {
                         <div className="form-grid">
                             <div className="main-data">
                                 <h3>Edit Article Information</h3>
-                                {['title', 'author', 'type', 'year', 'publisher', 'doi', 'url'].map((key) => (
+                                {[
+                                    'title', 'author', 'type', 'year', 'publisher', 'doi', 'url', 'tags', 'journal',
+                                    'booktitle', 'address', 'pages', 'volume', 'number', 'series', 'month', 'note',
+                                    'isbn', 'howpublished', 'organization', 'reference', 'abstract', 'keywords', 'cite'
+                                ].map((key) => (
                                     <div className="input-group" key={key}>
-                                        <label htmlFor={key}>{key}:</label>
+                                        <label htmlFor={key}>{key.charAt(0).toUpperCase() + key.slice(1)}:</label>
                                         <input
                                             type="text"
                                             id={key}
@@ -113,10 +136,23 @@ const BibTeXImportPage = () => {
                                         />
                                     </div>
                                 ))}
+                                <div className="input-group">
+                                    <label>
+                                        Are you the author?
+                                        <input
+                                            type="checkbox"
+                                            name="isCurrentUserOwner"
+                                            checked={isCurrentUserOwner}
+                                            onChange={() => setIsCurrentUserOwner(!isCurrentUserOwner)}
+                                        />
+                                    </label>
+                                </div>
                             </div>
                             <div className="additional-data">
                                 <h3>Additional Retrieved Data</h3>
-                                {additionalData && Object.keys(additionalData).filter(key => !['title', 'author', 'type', 'year', 'publisher', 'doi', 'url'].includes(key)).map((key) => (
+                                {additionalData && Object.keys(additionalData).filter(key => ![
+                                    'title', 'author', 'type', 'year', 'publisher', 'doi', 'url'
+                                ].includes(key)).map((key) => (
                                     <div className="input-group" key={key}>
                                         <label htmlFor={key}>{key}:</label>
                                         <input
